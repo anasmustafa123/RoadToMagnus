7b305543e33476d5a1ca8dfbeac2822cbe8a8bf5
Your Name <anasmuostafa@gmail.com>
2025-02-04 02:23:42 +0200

    fix unused errors

--- frontendTS/src/scripts/_CloudEvalWorker.ts
+++ frontendTS/src/scripts/_CloudEvalWorker.ts
@@ -1,5 +1,5 @@
 import type GameReviewManager from './_GameReviewManager';
-import { getCloudEvaluation } from '../api/lichessApiAccess';
+// import { getCloudEvaluation } from '../api/lichessApiAccess';
 
 class CloudEvalWorker {
   private game_review_manager: GameReviewManager;
@@ -10,7 +10,7 @@ class CloudEvalWorker {
     console.log('evaluating position');
     let continue_flag = true;
     while (!this.game_review_manager.done_evaluating() && continue_flag) {
-      const currentMove = this.game_review_manager.get_next_move();
+      /* const currentMove = this.game_review_manager.get_next_move();
       const { current_fen, move_num } = currentMove;
       const cloud_eval = await getCloudEvaluation(current_fen);
       if (!cloud_eval) continue_flag = false;
@@ -18,7 +18,7 @@ class CloudEvalWorker {
       console.warn({ cloud_eval });
       //console.debug({ engineLines });
       ui_update_callback();
-      this.game_review_manager.add_enginelines(cloud_eval, move_num);
+      this.game_review_manager.add_enginelines(cloud_eval, move_num); */
     }
     console.log('terminate_worker');
     return { success: true };
